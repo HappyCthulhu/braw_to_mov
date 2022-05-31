290a5cbd957cced468c8880eec7da673e9574c18
Валерий Газаров <v.gazarov@fortus.pro>
2022-05-31 11:37:39 +0300

    Remove import

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QDialog, QFileDialog
+
+from PyQt6.QtWidgets import QApplication, QDialog
+
 from ui_imagedialog import Ui_ImageDialog
 
 app = QApplication(sys.argv)
 window = QDialog()
+
 ui = Ui_ImageDialog()
 ui.setupUi(window)
-ui.test()
 
 window.show()
 sys.exit(app.exec())
