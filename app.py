c018914d883dd7a0f02b3c19a4bd3e3114de4aae
Валерий Газаров <v.gazarov@fortus.pro>
2022-07-04 04:20:03 +0400

    Add slider for choosing convert direction

--- app.py
+++ app.py
@@ -13,32 +13,51 @@ class MainController:
         self.ui = ui
         self.connect_slots()
         self.music_status = False
+        self.formatting_direction = 0
 
     def connect_slots(self):
         self.ui.select_button.clicked.connect(self.select_files)
         self.ui.convert_button.clicked.connect(self.rename_files)
         self.ui.speaker_button.clicked.connect(self.control_music)
+        self.ui.format_slider.valueChanged.connect(self.define_format)
+
+    def define_format(self):
+        self.formatting_direction = self.ui.format_slider.value()
+        print(self.formatting_direction)
 
     def select_files(self):
-        f_ps, _ = QFileDialog.getOpenFileNames(caption='Navigation', filter=".braw files (*.braw);;All Files (*)")
+        if self.ui.format_slider.value() == 0:
+            self.format_from = '.braw'
+            self.format_to = '.mov'
+        else:
+            self.format_from = '.mov'
+            self.format_to = '.braw'
 
-        if self.check_if_only_braw_files(f_ps) and f_ps:
+        f_ps, _ = QFileDialog.getOpenFileNames(caption='Navigation',
+                                               filter=f"{self.format_from} files (*{self.format_from});;All Files (*)")
+
+        if self.check_if_input_files_have_correct_format(f_ps, self.format_from) and f_ps:
             f_names = self.display_files(f_ps)
             f_names_with_brackets = ', '.join([f'"{f_name}"' for f_name in f_names])
             self.ui.files_label.setText(f_names_with_brackets)
             self.ui.convert_button.setEnabled(True)
         else:
+            self.ui.files_label.setStyleSheet("color: rgb(244, 244, 244);\n"
+                                              "font-weight: normal;")
             self.ui.convert_button.setEnabled(False)
 
-    def check_if_only_braw_files(self, f_ps):
+    def check_if_input_files_have_correct_format(self, f_ps, format_from):
 
-        not_braw = [fp for fp in f_ps if not fp.endswith('.braw')]
+        not_needed_format = [fp for fp in f_ps if not fp.endswith(format_from)]
 
-        if not_braw:
-            f_names = self.display_files(not_braw)
+        if not_needed_format:
+            f_names = self.display_files(not_needed_format)
             f_names_with_brackets = ', '.join([f'"{f_name}"' for f_name in f_names])
-            self.ui.files_label.setText(f'These files are not .braw:\n'
+
+            self.ui.files_label.setText(f'❗These files are not {format_from}:❗\n'
                                         f'{f_names_with_brackets}')
+            # TODO: не могу заставить это менять цвет:
+            # self.ui.files_label.setStyleSheet("color: rgb(0, 0, 0)")
             return False
 
         return True
@@ -51,7 +70,7 @@ class MainController:
     def rename_files(self):
         for old_name in self.f_names:
             old_fp = Path(self.dp).joinpath(old_name)
-            new_fp = f'{Path(self.dp).joinpath(old_name.split(".")[0])}.mov'
+            new_fp = f'{Path(self.dp).joinpath(old_name.split(".")[0])}{self.format_to}'
 
             os.rename(old_fp, new_fp)
         self.ui.files_label.setText('Success!')
